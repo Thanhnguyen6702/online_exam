00193363eac9462bc47bd4c80cf14f9bdb951d20
thanhnguyen <thanhnguyen6702@gmail.com>
2023-11-25 15:56:51 +0700

    add theme

---
+++ src/theme/breakpoints.js
@@ -0,0 +1,15 @@
+// ----------------------------------------------------------------------
+
+// Định nghĩa các điểm phá vỡ trong responsive design
+
+const breakpoints = {
+  values: {
+    xs: 0,
+    sm: 600,
+    md: 900,
+    lg: 1200,
+    xl: 1536,
+  },
+};
+
+export default breakpoints;
