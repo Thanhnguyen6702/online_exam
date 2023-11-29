f0d1d8b412dc779897de5526d2590d6b96b89ddb
Thanh02022002 <thanhvipad2002@gmail.com>
2023-11-29 14:07:13 +0700

    dashboard

--- server/src/configs/cache.js
+++
@@ -1,6 +0,0 @@
-const redis = require('redis');
-const { endpointUri, password } = require('../configs/config').redis;
-
-const redisClient = password ? redis.createClient({ url: `redis://${endpointUri}`, password }) : redis.createClient();
-
-module.exports = redisClient;
--- server/src/services/index.js
+++
@@ -1,5 +0,0 @@
-module.exports.authService = require('./auth.service');
-module.exports.userService = require('./user.service');
-module.exports.questionService = require('./question.service');
-module.exports.testService = require('./test.service');
-module.exports.answerSheetService = require('./answerSheet.service');
