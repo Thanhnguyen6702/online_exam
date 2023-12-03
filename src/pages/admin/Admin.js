132be35315818bec8db0f7e85b51c6e5d1b3d6de
thanhnguyen <thanhnguyen6702@gmail.com>
2023-12-03 20:44:20 +0700

    page admin

---
+++ src/pages/admin/Admin.js
@@ -0,0 +1,39 @@
+// @mui
+import { Container, Grid } from '@mui/material';
+// hooks
+import useSettings from '../../hooks/useSettings';
+// components
+import Page from '../../components/Page';
+import LinkWidget from '../../components/LinkWidget';
+// paths
+import { PATH_ADMIN, PATH_LEARNING } from '../../routes/paths';
+// ----------------------------------------------------------------------
+
+export default function Admin() {
+  const { themeStretch } = useSettings();
+
+  return (
+    <Page title="Bảng điều khiển của Admin">
+      <Container maxWidth={themeStretch ? false : 'xl'}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6} xl={3}>
+            <LinkWidget
+              title="Thêm câu hỏi"
+              link={PATH_LEARNING.question.create}
+              description="trắc nghiệm"
+              icon="eva:list-fill"
+            />
+          </Grid>
+          <Grid item xs={12} md={6} xl={3}>
+            <LinkWidget
+              title="Tạo đề thi"
+              link={PATH_LEARNING.test.create}
+              description="từ các câu hỏi đã chọn"
+              icon="eva:file-add-fill"
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </Page>
+  );
+}
