e5e24a47bb235f991a846d3f41af87411a50022c
thanhnguyen <thanhnguyen6702@gmail.com>
2023-12-03 20:00:06 +0700

    create register and login

---
+++ src/sections/auth/login/LoginForm.js
@@ -0,0 +1,92 @@
+import * as Yup from 'yup';
+import { useState } from 'react';
+// form
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+// @mui
+import { Alert, Button, Stack } from '@mui/material';
+// routes
+// hooks
+import useAuth from '../../../hooks/useAuth';
+import useIsMountedRef from '../../../hooks/useIsMountedRef';
+// components
+import Iconify from '../../../components/Iconify';
+import { FormProvider } from '../../../components/hook-form';
+
+// ----------------------------------------------------------------------
+
+export default function LoginForm() {
+  const { login, fbLogin, ggLogin } = useAuth();
+
+  const isMountedRef = useIsMountedRef();
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string().email('Email must be a valid email address').required('Email is required'),
+    password: Yup.string().required('Password is required'),
+  });
+
+  const defaultValues = {
+    email: '',
+    password: '',
+    remember: true,
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(LoginSchema),
+    defaultValues,
+  });
+
+  const { reset, setError, handleSubmit } = methods;
+
+  const onSubmit = async (data) => {
+    try {
+      await login(data.email, data.password);
+    } catch (error) {
+      console.error({ ...error });
+      reset();
+      if (isMountedRef.current) {
+        setError('afterSubmit', error);
+      }
+    }
+  };
+
+  const onFbLogin = async () => {
+    try {
+      await fbLogin();
+    } catch (error) {
+      console.error(error);
+      if (isMountedRef.current) {
+        setError('afterSubmit', error);
+      }
+    }
+  };
+
+  const onGgLogin = async () => {
+    try {
+      await ggLogin();
+    } catch (error) {
+      console.error(error);
+      if (isMountedRef.current) {
+        setError('afterSubmit', error);
+      }
+    }
+  };
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={2}>
+        <Button
+          fullWidth
+          startIcon={<Iconify icon="akar-icons:google-fill" />}
+          size="large"
+          variant="contained"
+          onClick={onGgLogin}
+        >
+          Đăng nhập bằng tài khoản Google
+        </Button>
+      </Stack>
+    </FormProvider>
+  );
+}
